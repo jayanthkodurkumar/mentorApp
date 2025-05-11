205f8154801f4f7694467eacd8fb7dfb7cb9750b
jayanthkkumar <jayanth@oleilabs.com>
2025-05-11 12:36:56 -0700

    fix 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
-import autoCert from "anchor-pki/auto-cert/integrations/next";
-const withAutoCert = autoCert({
-  enabledEnv: "development",
-});
 
 const nextConfig = {};
 
-export default withAutoCert(nextConfig);
+export default nextConfig;
